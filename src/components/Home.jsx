8b6efe4ae25ddc7318da7dc72c50e1eadfc6d7c3
Mahan Maleki <mahanmaleki950@gmail.com>
2025-10-24 16:59:20 +0330

    create most-sale products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,19 +11,27 @@ function Home() {
                     <div className="col-12 col-lg-8">
                         <Slider />
                     </div>
-                    <div className="col-4 d-none d-lg-block">
+                    <div className="col-12 col-lg-4 d-lg-block">
                         <div className="row">
                             <div className="col-12 d-flex align-items-center">
                                 <div className={css.pointer}></div>
                                 <h3 className='me-2'>جدیدترین <span className='border-bottom border-4 bold border-primary-subtle'>ایونت ها</span></h3>
                             </div>
                             <div className="col-12 mt-4">
-                                <div className="row">
-                                    <EventCard image="https://picsum.photos/800/400?3" title="جشنواره پائیزان - خرید به صرفه کالا های فصلی" link="#" />
+                                <div className="row d-flex justify-content-center justify-content-lg-start">
+                                    <EventCard image="https://picsum.photos/800/400?2" title="جشنواره پائیزان - خرید به صرفه کالا های فصلی" link="#" />
                                     <EventCard image="https://picsum.photos/800/400?4" title="جشنواره خلیج فارس - خرید محصولات بومی مناطق خلیج فارس" link="#" />
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid mt-4">
+                <div className="row p-2">
+                    <div className="col-12 d-flex align-items-center">
+                        <div className={css.pointer}></div>
+                        <h3 className='me-2'>پرفروش ترین <span className='border-bottom border-4 bold border-primary-subtle'>محصولات</span></h3>
                     </div>
                 </div>
             </div>
