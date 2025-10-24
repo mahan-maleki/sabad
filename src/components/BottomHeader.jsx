e8a80e368a9fd8dc3bc97af46364b25de7d25667
Mahan Maleki <mahanmaleki950@gmail.com>
2025-10-24 15:16:31 +0330

    fix responsibility of BottomHeader

--- src/components/BottomHeader.jsx
+++ src/components/BottomHeader.jsx
@@ -8,21 +8,12 @@ function BottomHeader() {
         <>
             <div className="container-fluid bg-body-tertiary d-none d-lg-block">
                 <div className="row p-3 d-flex align-items-center">
-                    <div className="col-4">
-                        <ul className='mb-0 d-flex justify-content-evenly'>
+                    <div className="col-12">
+                        <ul className='mb-0 d-flex justify-content-start'>
                             <li className={css.list}><NavLink to={""} className={"text-black text-decoration-none"}>راه های ارتباطی</NavLink></li>
                             <li className={css.list}><NavLink to={""} className={"text-black text-decoration-none"}>اشتراک سازمانی</NavLink></li>
                             <li className={css.list}><NavLink to={""} className={"text-black text-decoration-none"}>دریافت کارت اشتراک</NavLink></li>
                         </ul>
-                    </div>
-                    <div className="col-8">
-                        <div className="row d-flex justify-content-end">
-                            <div className="col-3 d-flex justify-content-end align-items-center">
-                                <span>صنف های ترند: </span>
-                            </div>
-                            <Trend title="رستوران" />
-                            <Trend title="میوه" />
-                        </div>
                     </div>
                 </div>
             </div>
