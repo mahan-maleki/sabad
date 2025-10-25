2de2f601b29b5d7e111575f4e043d6da932c537e
Mahan Maleki <mahanmaleki950@gmail.com>
2025-10-25 15:39:33 +0330

    change style of Rate

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,13 +5,9 @@ import { Star } from 'lucide-react';
 
 function ProductCard(props) {
 
-    const Card = styled.div`
-        height: 400px !important;
-    `;
-
     return (
         <>
-            <Card className="col-12 col-lg-2 mx-2 border-1 border-dark border border-opacity-10 p-2 rounded-3 mb-2">
+            <div className="col-12 col-md-4 col-lg-2 mx-2 border-1 border-dark border border-opacity-10 p-2 rounded-3 mb-2">
                 <NavLink className={"text-decoration-none text-dark"} to={props.link}>
                     <div className="row">
                         <div className="col-12">
@@ -19,14 +15,14 @@ function ProductCard(props) {
                         </div>
                         <div className="col-12 px-3">
                             <h3 className='fs-5'>{props.title}</h3>
-                            <div className='d-flex justify-content-between align-items-center'>
-                                <p className='price fs-4'>{props.price}<span className='price me-2 fs-6 text-secondary'>تومانءء</span></p>
-                                <div className='price d-flex align-items-center'><span className='price text-dark ms-1'>{props.rate}</span><Star color='#E18236' size={15} /></div>
+                            <div>
+                                <p className='price fs-5'>{props.price}<span className='price me-2 fs-6 text-secondary'>تومانءء</span></p>
                             </div>
+                            <div className='price d-flex align-items-center'><span className='price text-dark ms-1'>{props.rate}</span><Star color='#E18236' size={15} /></div>
                         </div>
                     </div>
                 </NavLink>
-            </Card>
+            </div>
         </>
     )
 }
