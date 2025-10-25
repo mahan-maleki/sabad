33c8e0eb4d72047700d3b2ae26ac95f071a24d83
Mahan Maleki <mahanmaleki950@gmail.com>
2025-10-25 15:57:39 +0330

    change some styles from more component

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -5,13 +5,13 @@ import { NavLink } from 'react-router'
 function More(props) {
     return (
         <>
-            <div className="col-12 col-md-6 col-lg-1 bg-body-secondary d-flex justify-content-center align-items-center rounded-3">
+            <div className="col-12 col-md-4 col-lg-1 bg-body-secondary d-flex justify-content-center align-items-center rounded-3 p-4 p-lg-3">
                 <NavLink to={props.to} className="text-decoration-none link-dark" >
                     <div className="row d-flex justify-content-center align-items-center">
                         <div className='d-flex justify-content-center mb-4'>
                             <SquareArrowLeft size={50} color='#AAAAAA' strokeWidth={1.25} />
                         </div>
-                        <h4 className="fs-4 text-center">مشاهده بیشتر</h4>
+                        <h4 className="fs-6 text-center">مشاهده بیشتر</h4>
                     </div>
                 </NavLink>
             </div>
